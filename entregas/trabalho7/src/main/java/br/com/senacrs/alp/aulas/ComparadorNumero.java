94d707eca17a8aeee027d3a4b1b641563cbc489e
631020198 <631020198@Sala603-02.fspoaeduc.com.br>
2013-04-11 22:07:25 -0300

    1

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparadorNumero.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparadorNumero.java
@@ -4,18 +4,20 @@ import java.util.Comparator;
 
 public class ComparadorNumero implements Comparator<Integer>{
 
+	
 	@Override
 	public int compare(Integer o1, Integer o2) {
 		
-		int par = 0;
-		
-		if (o1 % 2 == 0) {
-			par = o1;
-		} else {
-			par = o2;
+		if (o1 % 2 == 0 && o2 % 2 == 0) {
+			if (o1 > o2) {
+				return o1.compareTo(o2);
+			}else {
+				return o2.compareTo(o1);}
+		} else if (o1 < o2) {
+			return o2.compareTo(o1);
 		}
+		else{return o2.compareTo(o1);}
 		
-		return par;
 	}
 
 }
