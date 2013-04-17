7d094e5b2b9e091d0e47511f59ff9010b2f7857a
andriusgottems <andriusgottems@gmail.com>
2013-04-16 21:17:40 -0300

    correção

--- entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparadorNumero.java
+++ entregas/trabalho7/src/main/java/br/com/senacrs/alp/aulas/ComparadorNumero.java
@@ -4,20 +4,49 @@ import java.util.Comparator;
 
 public class ComparadorNumero implements Comparator<Integer>{
 
+	private int resultado;
 	
 	@Override
 	public int compare(Integer o1, Integer o2) {
+		//0 igual 
+		//-1
+		//1
+	int resultado = 0;	
 		
-		if (o1 % 2 == 0 && o2 % 2 == 0) {
-			if (o1 > o2) {
-				return o1.compareTo(o2);
-			}else {
-				return o2.compareTo(o1);}
-		} else if (o1 < o2) {
-			return o2.compareTo(o1);
+	if (par(o1) == par(o2)){
+		resultado = compareMesmaParidade(o1,o2);
+	} else {
+		resultado = compareParidadeDistinta(o1,o2);
+	}
+	
+	return resultado;
+	}
+	
+	
+	private boolean par(Integer o1) {
+		
+		boolean resultado = false;
+		resultado = o1.intValue() % 2 == 0;
+		return resultado;
+	}
+	
+	private int compareMesmaParidade(Integer o1, Integer o2){
+		
+		int resultado = 0;
+		
+		resultado = o1.intValue() - o2.intValue();
+		return resultado;
+		 
+	}
+	
+	private int compareParidadeDistinta(Integer o1, Integer o2){
+		
+		if (par(o1)){
+			return -1;
+		} else {
+			return 1;
 		}
-		else{return o2.compareTo(o1);}
 		
 	}
-
-}
+	
+	}
