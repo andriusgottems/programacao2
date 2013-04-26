2b881153b203cf71e5a5a3a353b47c55f620116a
631020198 <631020198@Sala603-02.fspoaeduc.com.br>
2013-04-25 21:03:13 -0300

    commit

--- exercicios/exercicio_arquivo/src/main/java/br/com/senacrs/alp/aulas/LerArquivos.java
+++ exercicios/exercicio_arquivo/src/main/java/br/com/senacrs/alp/aulas/LerArquivos.java
@@ -1,6 +1,7 @@
 package br.com.senacrs.alp.aulas;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,17 +13,19 @@ public class LerArquivos implements LeitorArquivos{
     public String[] lerArquivo(String arquivo) {
 
         BufferedReader br = null;
-        String linhaAtual;
+        String linhaAtual = null;
         List<String> linhas = new ArrayList<String>();
+        String [] resultado = null;
        
         try {
- 
-            br = new BufferedReader(new FileReader(arquivo));
+        	
+        	br = new BufferedReader(new FileReader(arquivo));
+        	
+        	//verificarExistencia(br);
  
             while ((linhaAtual = br.readLine()) != null) {
                
                 linhas.add(linhaAtual);
-                System.out.println(linhaAtual);
             }
            
         } catch (IOException e) {
@@ -35,15 +38,72 @@ public class LerArquivos implements LeitorArquivos{
             }
         }
        
-        String [] resultado = linhas.toArray(new String[linhas.size()]);
+         resultado = new String[linhas.size()];
+         resultado = linhas.toArray(resultado);
         return resultado;
     }
 
-    @Override
+   /* private void verificarExistencia(File arquivo) {
+
+    	if (arquivo == null) {
+    		throw new IllegalArgumentException();
+    	}
+    	if (arquivo.exists()) {
+    		throw new IllegalArgumentException();
+    	}
+    	if (arquivo.isFile()) {
+    		throw new IllegalArgumentException();
+    	}
+    	if (arquivo.canRead()) {
+    		throw new IllegalArgumentException();
+    	}
+		
+	}*/
+
+	@Override
     public String[] lerArquivoComSubstituicao(String arquivo, String busca,
             String substituicao) {
-        // TODO Auto-generated method stub
-        return null;
+		
+		BufferedReader br = null;
+        String linhaAtual = null;
+        List<String> linhas = new ArrayList<String>();
+        String [] resultado = null;
+        String [] auxiliar  = null;
+       
+        try {
+        	
+        	br = new BufferedReader(new FileReader(arquivo));
+        	
+        	while ((linhaAtual = br.readLine()) != null) {
+               
+                linhas.add(linhaAtual);
+                System.out.println(linhaAtual);
+            }
+        	
+        	auxiliar = new String[linhas.size()];
+        	auxiliar = linhas.toArray(resultado);
+        	resultado = new String[linhas.size()];
+        	
+            for (int i = 0; i < auxiliar.length; i++){
+            	
+            	auxiliar[i].replace(busca, substituicao);
+            	resultado[i]= auxiliar[i];
+            	System.out.println(resultado[i]);
+            }
+            
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+       
+         
+        return resultado;
     }
 
 }
