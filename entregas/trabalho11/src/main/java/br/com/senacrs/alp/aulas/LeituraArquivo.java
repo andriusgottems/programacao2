ed753ad6da708a6bb9db5eff03ac06ada70177d3
631020198 <631020198@Sala603-02.fspoaeduc.com.br>
2013-06-04 21:07:36 -0300

    commit

---
+++ entregas/trabalho11/src/main/java/br/com/senacrs/alp/aulas/LeituraArquivo.java
@@ -0,0 +1,29 @@
+package br.com.senacrs.alp.aulas;
+
+public class LeituraArquivo implements ArquivoConfiguracao {
+	
+	@Override
+	public String getRootDir() {
+		
+		String resultado = null;
+		
+		return resultado;
+	}
+
+	@Override
+	public String getErrorDir() {
+		
+		String resultado = null;
+		
+		return resultado;
+	}
+
+	@Override
+	public int getPort() {
+
+		Integer resultado = 0;
+		
+		return resultado;
+	}
+
+}
