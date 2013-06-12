0faafd8a8a2c8639fc83cc65b249506518ec7745
631020198 <631020198@Sala603-02.fspoaeduc.com.br>
2013-06-11 22:01:48 -0300

    commit

---
+++ entregas/trabalho13/src/main/java/br/com/senacrs/alp/aulas/MeuValidarRequisicao.java
@@ -0,0 +1,13 @@
+package br.com.senacrs.alp.aulas;
+
+import java.io.Reader;
+
+public class MeuValidarRequisicao implements ValidarRequisicaoGet {
+
+	@Override
+	public boolean ehRequisicaoGetValida(Reader requisicao) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
