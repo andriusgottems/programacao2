1332044fba42f6a86a35b21feb4def16428ccbd9
631020198 <631020198@Sala603-02.fspoaeduc.com.br>
2013-06-13 20:30:34 -0300

    ajuste teste

--- entregas/trabalho14/src/test/java/br/com/senacrs/alp/aulas/ObterPathRequisicaoGetTest.java
+++ entregas/trabalho14/src/test/java/br/com/senacrs/alp/aulas/ObterPathRequisicaoGetTest.java
@@ -50,7 +50,7 @@ public class ObterPathRequisicaoGetTest {
 		String abs = null;
 
 		path = "/";
-		abs = PWD + path.replaceAll("/", File.separator);
+		abs = PWD + path.replace("/", File.separator);
 		requisicao = getRequisicao(path);
 		resultado = obj.caminhoAbsoluto(requisicao);
 		Assert.assertEquals(path, abs, resultado);
